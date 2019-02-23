9cee01ddab6db32d9735822768cf5af186829578
João <joao.yuki@gmail.com>
2019-02-22 22:18:43 -0300

    Adicionando Lambda no código

--- src/br/com/joaofra/Ordenando.java
+++ src/br/com/joaofra/Ordenando.java
@@ -6,54 +6,80 @@ import java.util.List;
 import java.util.function.Consumer;
 
 public class Ordenando {
-	
-	public static void main (String[] args) {
-		
+
+	public static void main(String[] args) {
+
 		List<String> palavras = new ArrayList<String>();
 		palavras.add("Alura");
 		palavras.add("Editora casa do código");
 		palavras.add("Caelum");
-		
+
 		System.out.println(palavras);
 
+		// Como o método sort recebe um comparator, podemos implementar a classe dentro
+		// do sort mesmo
+		palavras.sort(new Comparator<String>() {
 
-		Comparator<String> comparando = new ComparandoPorTamanho();
-		palavras.sort(comparando);
+			@Override
+			public int compare(String s1, String s2) {
+				if (s1.length() > s2.length()) {
+					return 1;
+				}
+
+				if (s1.length() < s2.length()) {
+					return -1;
+				}
+
+				return 0;
+			}
+		});
+
+		// Passando para lambda
+		palavras.sort((String s1, String s2) -> {
+			if (s1.length() > s2.length()) {
+				return 1;
+			}
+
+			if (s1.length() < s2.length()) {
+				return -1;
+			}
+
+			return 0;
+		});
+
 		System.out.println(palavras);
-		
-		Consumer<String> exibe = new Exibindo();
+
+		// Criamos uma classe interna para diminuir o tanto de código
+		Consumer<String> exibe = new Consumer<String>() {
+
+			@Override
+			public void accept(String t) {
+				System.out.println(t);
+
+			}
+		};
+
 		palavras.forEach(exibe);
+
+		// Podemos usar diretamente no forEach também
+		palavras.forEach(new Consumer<String>() {
+
+			@Override
+			public void accept(String t) {
+				System.out.println(t);
+
+			}
+		});
+
+		// Já que o forEach recebe um Consume, e a interface Consumer só ter o metodo
+		// accept, pode fazer assim
+		// Podemos usar diretamente no forEach também
+		palavras.forEach((String t) -> {
+			System.out.println(t);
+		});
 		
+		palavras.forEach(t -> System.out.println(t));		
+
 	}
-	
-	
 
-}
-
-class Exibindo implements Consumer<String> {
-
-	@Override
-	public void accept(String t) {
-		System.out.println(t);
-		
-	}
-	
-}
-
-class ComparandoPorTamanho implements Comparator<String> {
-
-	@Override
-	public int compare(String s1, String s2) {
-		
-		if (s1.length() > s2.length()) {
-			return 1;
-		}
-		
-		if (s1.length() < s2.length()) {
-			return -1;
-		}		
-		
-		return 0;
-	}
-	
 }
